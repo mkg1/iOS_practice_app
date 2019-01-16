3c01f42e64d01e249f51e2e07f126e2bb49a9115
mkg1 <gehring.mg@gmail.com>
2019-01-15 23:17:31 -0500

    Initial button/text field set up

--- 08 Hours/ViewController.swift
+++ 08 Hours/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textField.becomeFirstResponder()
+        label.text = "What can you do with 8 Hours?"
     }
-
+    @IBAction func buttonWasTapped(_ sender: Any) {
+        label.text = "Hello \(textField.text!)"
+        textField.resignFirstResponder()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
