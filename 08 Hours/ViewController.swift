1d05f02b70d301617b429afd70a708b2a0858d74
mkg1 <gehring.mg@gmail.com>
2019-01-16 08:32:27 -0500

    Build text field and list

--- 08 Hours/ViewController.swift
+++ 08 Hours/ViewController.swift
@@ -8,15 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var groceryText: UITextField!
+    @IBOutlet weak var groceryTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         textField.becomeFirstResponder()
         label.text = "What can you do with 8 Hours?"
+        groceryTextView.delegate = self //have class viewController handle events for the grocery Text view
+    }
+
+    @IBAction func addItem(_ sender: Any) {
+        if let text = groceryText.text, text != "" {
+            groceryTextView.text.append("\(text)\n")
+            groceryText.text = ""
+            groceryText.resignFirstResponder()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        groceryText.resignFirstResponder()
+    }
+    
+    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+        textField.resignFirstResponder()
+        return false //avoid default behavior and block text view from being editable
     }
     @IBAction func buttonWasTapped(_ sender: Any) {
         label.text = "Hello \(textField.text!)"
